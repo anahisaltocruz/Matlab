e15d9d866d4136ae89301ff3cf3c67c8463c9f01
Anahi Salto L <anahisalto16@gmail.com>
2021-06-28 00:43:44 -0500

    T3, final de la sección 3

--- Programacion/EjemploIf.m
+++ Programacion/EjemploIf.m
@@ -1,3 +1,4 @@
+%% Sección 1
 a = 10;
 b = 20;
 if(b > a)
@@ -7,3 +8,9 @@ else
     disp('Dentro de else');
     disp('Dentro de else 2');
 end
+
+%% Sección 2
+disp('esta es la sección 2');
+
+%% s3
+disp('sección 3 c:');
